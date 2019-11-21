36c4b9f52c82c5b754519968a738f8ba5839d8c9
Ahmed Yasser <ahm0ed0yasser@gmail.com>
2019-11-21 18:59:53 +0200

    last commit by Ahmed YASSER

--- SimpleDBMS/src/eg/edu/alexu/csd/oop/db/cs24/Parser.java
+++ SimpleDBMS/src/eg/edu/alexu/csd/oop/db/cs24/Parser.java
@@ -1,5 +1,6 @@
 package eg.edu.alexu.csd.oop.db.cs24;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -77,7 +78,7 @@ public class Parser {
     private ArrayList<String > fieldArraygetter(String in){
         in=in.replace(","," , ");
         in=in.replace("="," = ");
-	    Pattern p=Pattern.compile("[^\\s]*");
+	    Pattern p=Pattern.compile("(['][^']*[']|[^'^\\s])*");
         ArrayList<String> a=new ArrayList<>();
         Matcher matcher=p.matcher(in);
         while (matcher.find()) {String j=matcher.group();
@@ -96,6 +97,14 @@ public class Parser {
             i++;
         }
         return output;
+    }
+    private int listcount(String in,Character a){
+	    int count=0,i=0;
+	    while(i<in.length()){
+	        if(in.charAt(i)==a)count++;
+	        i++;
+        }
+	    return count;
     }
 
     private ArrayList<String > selectquery(String in){
@@ -128,23 +137,28 @@ public class Parser {
     }
 
 
+
 	public Map<String,Object> updateQueryParser(String input){
-        if(input.charAt(input.length()-1)!=';'){return null;}
-        else{input=input.substring(0,input.length()-1);}
+	    Pattern a=Pattern.compile(
+	            "((update)|(UPDATE))[\\s]+[\\S]+[\\s]+((set)|(SET))[\\s]+[\\S]+[\\s]*[=][\\s]*(['][^']*[']|[^'])" +
+                        "([\\s]*([,][\\s]+[\\S]+[\\s]*[=][\\s]*(['][^']*[']|[^']))*)[\\s]*(((WHERE)|(where))[^;]+)*[;]"
+        );
+	    Matcher z=a.matcher(input.toLowerCase());
+	    if(!z.matches())return null;
+	    input=input.substring(0,input.length()-1);
 	    ConditionParser cp=ConditionParser.getInstance();
         Map<String,Object> output=new HashMap<>();
         int i=0,conditioni,seti,updatei;
         updatei=input.toLowerCase().indexOf("update");
         if(input.toLowerCase().contains("where")){conditioni=input.toLowerCase().indexOf("where");}
         else conditioni=-1;
-        if(input.toLowerCase().contains("set")){ seti=input.toLowerCase().indexOf("set");}
-        else return null;
+         seti=input.toLowerCase().indexOf("set");
         output.put("table",namegetter(input.substring(updatei,seti)));
         ArrayList<String> fieldarray=new ArrayList<>();
         if(conditioni!=-1){fieldarray=fieldArraygetter(input.substring(seti,conditioni));}
         else{fieldarray=fieldArraygetter(input.substring(seti));}
         while(i<fieldarray.size()){if(fieldarray==null)return null;
-            output.put(fieldarray.get(i).substring(0,fieldarray.get(i).indexOf('=')).replace(" ",""),fieldarray.get(i).substring(fieldarray.get(i).indexOf('=')));
+            output.put(fieldarray.get(i).substring(0,fieldarray.get(i).indexOf('=')).replace(" ",""),fieldarray.get(i).substring(fieldarray.get(i).indexOf('=')+1));
             i++;
         }
         if(conditioni==-1){output.put("condition",null);}
@@ -156,13 +170,17 @@ public class Parser {
 
     }
     public Map<String,Object> selectQueryParser(String input){ConditionParser cp=ConditionParser.getInstance();
-       if(input.charAt(input.length()-1)!=';'){return null;}
-       else{input=input.substring(0,input.length()-1);}
+        Pattern a=Pattern.compile(
+                "((select)|(SELECT))[\\s]+([*]|([^,\\s]+[\\s]*([,][\\s]*[^,\\s]+)*))[\\s]+((FROM)|(from))[\\s]+[\\S]+[\\s]+(((WHERE)|(where))[^;]+)*[;]"
+        );
+        Matcher z=a.matcher(input.toLowerCase());
+        if(!z.matches())return null;
+       input=input.substring(0,input.length()-1);
         Map<String,Object> output=new HashMap<>();
         int selecti,fromi,conditioni;
         selecti=input.toLowerCase().indexOf("select");
-        if(!input.toLowerCase().contains("from")){return  null;}
-        else{fromi=input.toLowerCase().indexOf("from");}
+
+        fromi=input.toLowerCase().indexOf("from");
         if(!input.toLowerCase().contains("where")){conditioni=-1;}
         else{conditioni=input.toLowerCase().indexOf("where");}
         if(selectquery(input.substring(selecti,fromi))==null)return null;
@@ -179,6 +197,100 @@ public class Parser {
             output.put("condition",cp.noregexparser(input.substring(conditioni+5)));}
         return output;
     }
+
+   public Map<String,Object> insertQueryParser(String input){
+        input.replace(")"," ) ");
+        input.replace("("," ( ");
+        input.replace(","," , ");
+        Pattern a=Pattern.compile(
+                "(insert)[\\s]+(into)[\\s]+[\\S]+[\\s]+(([\\s]*[\\(][\\s]*[^\\s,\\(\\)]+([\\s]*[,][\\s]*[^,\\s\\(\\)]+)*" +
+                        "[\\s]*[\\)][\\s]*(values)[\\s]*+[\\s]*[\\(][\\s]*(([^\\s,\\(\\)]+)|(['][^']+[']))([\\s]*[,][\\s]*" +
+                        "(([^,\\s]+)|(['][^']+['])))*[\\s]*[\\)][\\s]*)|([\\s]*(values)[\\s]*+[\\s]*[\\(][\\s]*(([^\\s,\\(\\)]+)" +
+                        "|(['][^']+[']))([\\s]*[,][\\s]*(([^,\\s\\(\\)]+)|(['][^']+['])))*[\\s]*[\\)][\\s]*))[;]"
+        );
+        Matcher z=a.matcher(input.toLowerCase());
+        if(!z.matches())return null;
+        else{input=input.substring(0,input.length()-1);}
+        input.replace(")"," ) ");
+        input.replace("("," ( ");
+        input.replace(","," , ");
+        Map<String,Object> output=new HashMap<>();
+        ArrayList<String> var=new ArrayList<>(),fields=new ArrayList<>();
+
+        if(listcount(input,'(')==1){
+                int i=input.indexOf("(");String temp=new String();
+                while(i<input.indexOf(")")){
+                    if(i==input.indexOf(")")-1){temp=temp+input.charAt(i);var.add(temp);}
+                    else if(input.charAt(i)!=' '&&input.charAt(i)!=','){
+                        temp=temp+input.charAt(i);
+                        if(input.charAt(i)=='\''){i++;
+                            while(input.charAt(i)!='\''){
+                                temp=temp+input.charAt(i);i++;
+                            }
+                            temp=temp+'\'';i++;var.add(temp);temp="";
+                        }
+                    }
+                    else if(input.charAt(i)==','){
+                        var.add(temp);
+                        temp= "";
+                    }
+                    i++;
+                }
+            for (int j=0;j<var.size();j++){
+                output.put(Integer.toString(j),var.get(j));
+            }
+
+        }
+
+        else if(listcount(input,'(')==2){String temp=new String();
+            int i=input.lastIndexOf('(')+1;
+            while(i<input.lastIndexOf(")")){
+                if(i==input.lastIndexOf(")")-1){temp=temp+input.charAt(i);var.add(temp);}
+                else if(input.charAt(i)!=' '&&input.charAt(i)!=','){
+                    temp=temp+input.charAt(i);
+                    if(input.charAt(i)=='\''){i++;
+                        while(input.charAt(i)!='\''){
+                            temp=temp+input.charAt(i);i++;
+                        }
+                        temp=temp+'\'';i++;var.add(temp);temp="";
+                    }
+                }
+                else if(input.charAt(i)==','){
+                    var.add(temp);
+                    temp= "";
+                }
+                i++;
+            }
+            i=input.indexOf("(")+1;
+            while(i<input.indexOf(")")){
+                if(i==input.indexOf(")")-1){temp=temp+input.charAt(i);fields.add(temp);}
+                else if(input.charAt(i)!=' '&&input.charAt(i)!=','){
+                    temp=temp+input.charAt(i);
+
+                }
+                else if(input.charAt(i)==','){
+                    fields.add(temp);
+                    temp= "";
+                }
+                i++;
+            }
+            if(var.size()!=fields.size()){return null;}
+            else {
+                for (int j=0;j<var.size();j++){
+                    output.put(fields.get(j),var.get(j));
+                }
+            }
+        }
+       int i= input.toLowerCase().indexOf("into")+4;
+        String temp="";
+        while(input.charAt(i)==' '){i++;}
+        while(!(input.charAt(i)==' ')){temp=temp+input.charAt(i);i++;}
+        output.put("table",temp);
+        output.put("condition",null);
+        return output;
+
+
+   }
 
 		
 }
